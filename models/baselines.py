7dbf4b7477631dd4ab118a9a8271f108344e1669
Loïc Omnes <loic.omnes@free.fr>
2022-05-20 10:11:11 +0200

    add sklearn generic baselines

--- models/baselines.py
+++ models/baselines.py
@@ -1,6 +1,9 @@
-from models.base_model import BaseModel
 import numpy as np
 import pandas as pd
+from models.base_model import BaseModel
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 
 
 class RandomModel(BaseModel):
@@ -20,9 +23,6 @@ class RandomModel(BaseModel):
         preds = np.random.randint(2, size=(N, self.n_class))
         return pd.DataFrame(preds, index=X.index, columns=self.target_cols)
 
-    def predict(self, X):
-        return self.predict_proba(X)
-
 
 class LabelMean(BaseModel):
     """Model that predicts the label mean value for each label"""
@@ -40,6 +40,42 @@ class LabelMean(BaseModel):
         preds = np.vstack([self.means for _ in range(X.shape[0])])
         return pd.DataFrame(preds, index=X.index, columns=self.target_cols)
 
-    def predict(self, X):
-        preds = self.predict_proba(X)
-        return 1. * (preds >= 0.5)
+
+class SklearnModel(BaseModel):
+    """Model based on a sklearn classifier"""
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+        self.preprocessor = None
+        self.target_cols = None
+
+    def fit(self, X, y):
+        if self.preprocessor is not None:
+            X = self.preprocessor.fit_transform(X)
+        self.model.fit(X, y)
+        self.target_cols = y.columns
+        return self
+
+    def predict_proba(self, X):
+        if self.preprocessor is not None:
+            X = self.preprocessor.transform(X)
+        preds = self.model.predict(X)
+        return pd.DataFrame(preds, index=X.index, columns=self.target_cols)
+
+
+class RF(SklearnModel):
+    """Random forest classifier"""
+    def __init__(self):
+        super().__init__(RandomForestClassifier())
+
+
+class KNN(SklearnModel):
+    """K-nearest neighbors classifier"""
+    def __init__(self):
+        super().__init__(KNeighborsClassifier())
+
+
+class MLP(SklearnModel):
+    """Multi-layer perceptron classifier"""
+    def __init__(self):
+        super().__init__(MLPClassifier())
