0250d1e21f17187e5b8cacda26af169375da7820
Loïc Omnes <loic.omnes@free.fr>
2022-05-20 03:36:39 +0200

    add command line

---
+++ cli.py
@@ -0,0 +1,39 @@
+import click
+from utils.evaluate import SequencesEvaluator, ExpressionsEvaluator, SequencesExpressionsEvaluator
+from models.expressions.baselines import *
+from models.sequences.baselines import *
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command(name='eval',
+               context_settings={'ignore_unknown_options': True,
+                                 'allow_extra_args': True})
+@click.argument('model_cls')
+@click.pass_context
+def eval(ctx, model_cls):
+    try:
+        model_cls = globals()[model_cls]
+    except KeyError:
+        raise Warning(f'Unknown model class {model_cls} ! Try importing it in cli.py')
+
+    kwargs = {}
+    for i in range(0, len(ctx.args), 2):
+        key, value = ctx.args[i], ctx.args[i + 1]
+        key = key.lstrip('-')
+        value = float(value)
+        kwargs[key] = value
+
+    model = model_cls(**kwargs)
+    evaluator_cls = SequencesExpressionsEvaluator if 'sequences_expressions' in model_cls.__module__ \
+                    else SequencesEvaluator if 'sequences' in model_cls.__module__ \
+                    else ExpressionsEvaluator
+    evaluator_cls(model).evaluate()
+
+
+cli.add_command(eval)
+if __name__ == '__main__':
+    cli()
