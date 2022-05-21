3cc0a8451d63ff61afd9701659d7297570aba10b
clementbernardd <clementbernardd@gmail.com>
2022-05-21 13:29:54 +0200

    update(gif)

--- api/reusable_components.py
+++ api/reusable_components.py
@@ -4,16 +4,17 @@ import os
 
 
 def get_logo():
-    img = cv2.imread(os.path.join('images', 'logo.png'))
+    img = cv2.imread(os.path.join('images', 'logo_blanc.png'))
     col1, col2, col3 = st.columns([0.2, 0.4, 0.2])
     col2.image(img, use_column_width=True)
 
 
 def get_app_info():
-    img = cv2.imread(os.path.join('images', 'logo.png'))
+    img = cv2.imread(os.path.join('images', 'logo_blanc.png'))
     st.sidebar.image(img, use_column_width=True)
     st.sidebar.info(
         """
          The present project consists on the development of an AI method based on deep-learning to associate long non-coding RNAs (lncRNAs) to cancer types and biological functions.
         """)
+
     return
